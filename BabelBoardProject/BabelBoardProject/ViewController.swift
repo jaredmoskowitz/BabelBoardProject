de7d4c7057a82557b75e05e0c3e7ed1dd2274d5e
Jared Moskowitz <jaredmoskowitz@me.com>
2014-09-20 04:41:05 -0400

    keyboard working

--- BabelBoardProject/BabelBoardProject/ViewController.swift
+++ BabelBoardProject/BabelBoardProject/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
