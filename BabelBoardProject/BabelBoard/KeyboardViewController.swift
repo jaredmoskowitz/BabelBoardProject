5d3ccb58b8246b7c4ba2e2383b9bb7c68d071c20
syntactix <ccrunner2013@gmail.com>
2014-09-20 20:26:45 -0400

    add mathurl to keyboard

--- BabelBoardProject/BabelBoard/KeyboardViewController.swift
+++ BabelBoardProject/BabelBoard/KeyboardViewController.swift
@@ -278,6 +278,14 @@ class KeyboardViewController: UIInputViewController {
         }
     }
     
+    func math(message: String) -> String? {
+        myparams = ["X-Parse-Application-Id": "u19o03YiCWzeonVWaTNueubVC8UupUiP7HVibWF1", "X-Parse-REST-API-Key": "BY0NkbNymGC0n0pK3TicPHIosksEdK2DG8M1uCzE", "Content-Type": "application/json", "message": message]
+        Alamofire.request(.GET, "https://api.parse.com/1/functions/math/get", parameters: myparams)
+            .responseJSON { (request, response, string, error) in
+                println(string)
+        }
+    }
+    
     
     override func textWillChange(textInput: UITextInput) {
         // The app is about to change the document's contents. Perform any preparation here.
